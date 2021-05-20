84b29657850a0c9d6cac383e0c2e40ee3f81cc3d
Leonardo <leonardofasolato00@gmail.com>
2021-05-20 09:28:59 +0200

    fix code AutoRedirectTimer

--- static/js/formSubmitted.js
+++ static/js/formSubmitted.js
@@ -1,13 +1,14 @@
 let count = 5;
-setInterval(function () {
+let countDownRedirect = setInterval(function () {
     count--;
     document.getElementById('countDown').innerHTML = "Sarai riportato alla home in " + count + " secondi";
 
     if (count === 1)
         document.getElementById('countDown').innerHTML = "Sarai riportato alla home in " + count + " secondo";
-    else if (count === 0)
-        window.location.href = '/';
-    else if (count < 0)
-        document.getElementById('countDown').innerHTML = "Sarai riportato alla home in 0 secondi";
+
+    if (count === 0){
+       window.location.href = '/';
+       clearInterval(countDownRedirect);
+    }
 
 }, 1000);
